90b2e23baeaf0462b19a123a50b418d99d2e90f0
supreet <supreetsingh1399@gmail.com>
2024-04-04 23:17:23 +0530

    Refactor

---
+++ webpage/assignment_webpage/Price_Calculator_books/books.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll('.I1, .I2, .I3');
+elements.forEach(element => {
+    element.addEventListener('click', function (event) {
+        // event.preventDefault();
+        this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
+    });
+});
+var total = 0;
+var button=document.getElementsByClassName('butn').addEventListener('click',()=>{
+    var forEach(element)===active{
+        if 
+    }
+};
