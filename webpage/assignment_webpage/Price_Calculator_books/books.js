3aa09e1476eedef62d7c96b689265973d080992d
supreet <supreetsingh1399@gmail.com>
2024-04-05 17:40:33 +0530

    Refactor CSS and HTML

--- webpage/assignment_webpage/Price_Calculator_books/books.js
+++ webpage/assignment_webpage/Price_Calculator_books/books.js
@@ -1,13 +1,6 @@
-const elements = document.querySelectorAll('.I1, .I2, .I3');
+const elements = document.querySelectorAll('.sub-container');
 elements.forEach(element => {
     element.addEventListener('click', function (event) {
-        // event.preventDefault();
         this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active');
     });
 });
-var total = 0;
-var button=document.getElementsByClassName('butn').addEventListener('click',()=>{
-    var forEach(element)===active{
-        if 
-    }
-};
