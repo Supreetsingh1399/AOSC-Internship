9573109a1c7da2e080b4569c2d09ab9232f3c70e
supreet <supreetsingh1399@gmail.com>
2024-06-07 02:18:42 +0530

    chore: Update MongoDB connection to use new mongoose version

--- node-js/mongodb-connect.js
+++ node-js/mongodb-connect.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 // MongoDB connection URL
 const url = 'mongodb://localhost:27017/mydatabase';
 
 // Connect to MongoDB
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url)
     .then(() => {
         console.log('Connected to MongoDB');
-        // Your code logic here
     })
     .catch((error) => {
         console.error('Failed to connect to MongoDB:', error);
