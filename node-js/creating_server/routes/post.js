daf8a434e43f4e4cf2b7042244a6ae263a06f450
supreet <supreetsingh1399@gmail.com>
2024-06-12 14:32:45 +0530

    chore:some changes

--- node-js/creating_server/routes/post.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const validator = require('../Validators');
-const postController = require('../controllers/post');
-const router = express.Router();
-
-router.get('/', postController.getPost);
-router.post('/post',validator.createPostValidator , postController.createPost);
-
-module.exports = router;
--- node-js/sum.js
+++
@@ -1,3 +0,0 @@
-const helper = require("./import-export");
-const total = helper.sum(10,20);
-console.log("Total:",total);
